bf6cb407dffa14c6e5f76ec67629a066916c2e84
andilutfi944-afk <andilutfi944@gmail.com>
2026-04-24 17:14:49 +0700

    menambahkan bool delNode

--- singly_linkedist.cpp
+++ singly_linkedist.cpp
@@ -86,4 +86,35 @@ public:
 
         return (*current != NULL);
     }
+
+    bool delNode(int nim)
+    {
+        Node *current, *previous;
+        if (!Search(nim, &previous, &current))
+            return false;
+        if (current == START)
+            START = START->next;
+        else
+            previous->next = current->next;
+        delete current;
+        return true;
+    }
+    void traverse()
+    {
+        if(ListEmpty())
+        {
+            cout << "\nList Kosong\n";
+        }
+        else
+        {
+            cout << "\nData didalam list adalah:\n";
+            Node *currentNode = START;
+            while (currentNode != NULL)
+            {
+                cout << currentNode->noMhs << endl;
+                currentNode = currentNode->next;
+            }
+            cout << endl;
+        }
+    }
 }
