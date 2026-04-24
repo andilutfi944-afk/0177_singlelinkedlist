c7423ad0ef35b4d3a210c7235bd95cdf2cd6398b
andilutfi944-afk <andilutfi944@gmail.com>
2026-04-24 17:12:40 +0700

    menambahkan bool listempty

--- singly_linkedist.cpp
+++ singly_linkedist.cpp
@@ -68,4 +68,22 @@ public:
         nodeBaru->next = current;
         previous->next = nodeBaru;
     }
+    bool ListEmpty()
+    {
+        return (START == NULL);
+    }
+    
+    bool Search(int nim, Node **previous, Node **current)
+    {
+        *previous = START;
+        *current = START;
+
+        while ((*current != NULL) && (nim != (*current)->noMhs))
+        {
+            *previous = *current;
+            *current = (*current)->next;
+        }
+
+        return (*current != NULL);
+    }
 }
